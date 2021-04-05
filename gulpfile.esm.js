6d8523a6bee5d385dca6e6cea06989f8835147cc
Mannar Hielal <mannar.hielal@liip.ch>
2021-04-05 21:42:40 +0200

    Install and run imagemin-webp and generate WebP images

--- gulpfile.esm.js
+++ gulpfile.esm.js
@@ -1,11 +1,13 @@
 import glob from 'glob';
 import gulp from 'gulp';
 import imagemin from 'gulp-imagemin';
+import imageminWebp from 'imagemin-webp';
+import rename from 'gulp-rename';
 import nunjucks from 'gulp-nunjucks';
 import rimraf from 'rimraf';
 
 function clean() {
-	return glob('./www/*.{html,jpg,svg}', {}, function(er, files) {
+	return glob('./www/*.{html,jpg,svg,webP}', {}, function(er, files) {
 		for(let file in files) {
 			rimraf(files[file], () => {});
 		}
@@ -26,7 +28,17 @@ function compressImages() {
 	.pipe(gulp.dest('www/img'))
 }
 
-const build = gulp.series(clean, compileNunjucks, compressImages);
+function createWebP() {
+	return gulp.src("www/img/**/*")
+	.pipe(imagemin([
+		imageminWebp()
+	]))
+	.pipe(rename(function (path) {
+		path.extname = ".webp";
+	}))
+	.pipe(gulp.dest('www/img'));
+}
+const build = gulp.series(clean, compileNunjucks, compressImages, createWebP);
 
 export {
 	build
